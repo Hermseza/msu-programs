ace5983bad4d7ec164bd918d8318993cdc4b106a
Hermseza <hermseza@gmail.com>
2015-11-02 11:20:53 -0600

    Create program_014_define_statement.cpp

---
+++ C_Programs/program_014_define_statement.cpp
@@ -0,0 +1,27 @@
+// AUTHOR	:  Zack Hermsen
+// DATE		:  10(October)-01(Wednesday)-2014
+// Course	:  Csci 180, Section A
+// Program	:  program_014_define_statement.cpp
+// Goal		:  To complete Lab 14
+// Purpose	:  To use const and keywords
+
+#include <stdio.h>
+
+
+int main()
+{
+	double r;
+	double area;
+	const double PI = 3.1415926
+
+	printf("\n Please enter the radius of a circle : ");
+	scanf_s("%lf", &r);
+
+	area = PI * r * r;
+	
+	printf("\n For a circle with radius %lf, the area is %lf", r, area);
+
+	getchar();
+	getchar();
+	return 0;
+}
