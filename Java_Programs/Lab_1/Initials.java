e9c8d02a48238d597a741f30f1308f5cbdf3dfd3
Hermseza <hermseza@gmail.com>
2016-04-14 14:11:28 -0500

    Added files via upload

---
+++ Java_Programs/Lab_1/Initials.java
@@ -0,0 +1,31 @@
+/**
+ * Filename		: Initials.java
+ * Purpose		: To create a simple program that prints my initials.
+ * Author		: Zackary Hermsen
+ * School		: McNeese State University
+ * email		: hermseza@gmail.com
+ * Instructor	: Dr. Kussman
+ * Due Date		: 08/27/2015
+ * Compiler		: Eclipse
+ * Executable	: Initials.jar
+ * History		: 08/27/2015 : Program Genesis 
+ * 				  08/28/2015 : Added Header
+ */
+
+
+package initialsPackage;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("******	      *          *     *");
+		System.out.println("    *	     *  *        *     *");
+		System.out.println("   *	    *    *       *     *");
+		System.out.println("  *        ********      *******");
+		System.out.println(" *        *        *     *     *");
+		System.out.println("******	 *          *    *     *");
+
+	}
+
+}
---
+++ Java_Programs/Lab_1/SillyFace.java
@@ -0,0 +1,31 @@
+/**
+ * Filename		: SillyFace.java
+ * Purpose		: To create a simple program that prints a silly face.
+ * Author		: Zackary Hermsen
+ * School		: McNeese State University
+ * email		: hermseza@gmail.com
+ * Instructor	: Dr. Kussman
+ * Due Date		: 08/27/2015
+ * Compiler		: Eclipse
+ * Executable	: SillyFace.jar
+ * History		: 08/27/2015 : Program Genesis  
+ * 				  08/28/2015 : Added Header
+ */
+
+
+package sillyFacePackage;
+
+public class SillyFace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("     /_/_/_/  ");
+		System.out.println("    |       | ");
+		System.out.println("   (| O   O |)  ");
+		System.out.println("    |  /_   |  ");
+		System.out.println("    |_ _ _ _| ");
+		System.out.println("    |_ _ _ _|  ");
+
+	}
+
+}
